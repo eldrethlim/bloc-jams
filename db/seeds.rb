5c45335e2bacdc70bd10afa5103fe2c6b4b2eb8b
Jun Xian <eldrethlim@gmail.com>
2014-10-22 10:25:14 +0000

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,26 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'faker'
 
-20.times do
+15.times do
   Album.create!(
     title: Faker::Lorem.sentence(3),
     author: Faker::Name.name,
     description: Faker::Lorem.sentence(3)
   )
 end
+
 albums = Album.all
 
-100.times do
-  Song.create!(
-    album: albums.sample,
-    title: Faker::Lorem.sentence(3),
-    number: Faker::Number.digit,
-    duration: Faker::Number.number(2)
-  )
+albums.each do |album|
+  random = rand(5..25)
+  x = 1
+  random.times do
+    Song.create!(
+      album: album,
+      title: Faker::Lorem.sentence(3),
+      number: x,
+      duration: Faker::Number.number(2)
+    )
+    x += 1
+  end
 end
